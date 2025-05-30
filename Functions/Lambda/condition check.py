03d6bbbc9f229591e2b18aa26389fec9a2237780
Yash Haldiya <yashasvi9199@gmail.com>
2025-05-30 20:46:08 +0530

    iterations with lambda

---
+++ Functions/Lambda/condition check.py
@@ -0,0 +1,3 @@
+n = lambda arg : 'Negative' if arg < 0 else 'Positive' if arg > 0 else 'Zero'
+
+print( n(45) )
---
+++ Functions/Lambda/lambda if-else.py
@@ -0,0 +1,4 @@
+check = lambda x: 'Even' if x%2==0 else 'Odd'
+
+print(check(45))
+print(check(20))
