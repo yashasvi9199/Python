1f317588d1402183c42061083ccdaef07d4eaf5e
Yash Haldiya <yashasvi9199@gmail.com>
2025-05-30 20:45:54 +0530

    introduction to lambda

---
+++ Functions/Lambda/basic lambda function.py
@@ -0,0 +1,14 @@
+# Convert string to upper class
+
+
+# General approach
+def upp(arg):
+    return arg.upper()
+
+
+sol = lambda args: args.upper()
+
+str = "hello world"
+
+print(upp(str))
+print(sol(str))
---
+++ Functions/Lambda/def vs lambda.py
@@ -0,0 +1,9 @@
+sqr = lambda n: n**2
+
+
+def sqrt(n):
+    return n**2
+
+
+print(sqr(11))
+print(sqrt(12))
