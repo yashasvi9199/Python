760bd8e600c9b0e71723a37ce025593bedfdfb41
Yash Haldiya <yashasvi9199@gmail.com>
2025-06-11 11:14:19 +0530

    Create import all methods in module

---
+++ Import in python/03 Import all methods.py
@@ -0,0 +1,9 @@
+from math import *
+
+# ? Imported all the method using asterisk*.
+# ? Non conventional method as it may cause confusion in usage of various methods from different modules
+
+print(pi)
+
+res = sqrt(9)
+print(res)
